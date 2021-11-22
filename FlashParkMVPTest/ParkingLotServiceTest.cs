76b55e98b3c5cb81dc657a13bb7d35cd854f544c
jtambe <tambe_jayesh@hotmail.com>
2021-11-22 07:27:56 -0800

    Async test platform needs to target framework 4.5. Sucks

--- FlashParkMVPTest/ParkingLotServiceTest.cs
+++ FlashParkMVPTest/ParkingLotServiceTest.cs
@@ -5,6 +5,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Moq;
 using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 namespace FlashParkMVPTest
 {
@@ -19,18 +23,39 @@ namespace FlashParkMVPTest
         }
 
         [TestMethod]
-        public async void GetParkingLotsAsyncTestCheckCount()
+        public async Task GetParkingLotsAsyncTestCheckCount()
         {
 
-            var data = new List<ParkingLot>() 
+            var data = new List<ParkingLot>()
             {
                 new ParkingLot() { ParkingLotId = 2, ParkingLotAddress = "Garage1" },
                 new ParkingLot() { ParkingLotId = 3, ParkingLotAddress = "Garage2" },
                 new ParkingLot() { ParkingLotId = 1, ParkingLotAddress = "Garage3" }
             }.AsQueryable();
 
-            var mockSet = new Mock<DbSet<ParkingLot>>();
-            mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.Provider).Returns(data.Provider);
+            //var mockSet = new Mock<DbSet<ParkingLot>>();
+            //mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.Provider).Returns(data.Provider);
+            //mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.Expression).Returns(data.Expression);
+            //mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            //mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            //var mockContext = new Mock<DBContext>();
+            //mockContext.Setup(c => c.ParkingLots).Returns(mockSet.Object);
+
+            //var service = new ParkingLotService(mockContext.Object);
+            //var lots = service.GetParkingLotsAsync().Result; 
+
+
+            //var mockSet = new Mock<System.Data.Entity.DbSet<ParkingLot>>();
+            var mockSet = new Mock<Microsoft.EntityFrameworkCore.DbSet<ParkingLot>>();
+            mockSet.As<IDbAsyncEnumerable<ParkingLot>>()
+                .Setup(m => m.GetAsyncEnumerator())
+                .Returns(new TestDbAsyncEnumerator<ParkingLot>(data.GetEnumerator()));
+
+            mockSet.As<IQueryable<ParkingLot>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestDbAsyncQueryProvider<ParkingLot>(data.Provider));
+
             mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.Expression).Returns(data.Expression);
             mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.ElementType).Returns(data.ElementType);
             mockSet.As<IQueryable<ParkingLot>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
@@ -42,6 +67,8 @@ namespace FlashParkMVPTest
             var lots = await service.GetParkingLotsAsync();
 
             Assert.AreEqual(3, lots.ToList().Count);
+
+            Assert.AreEqual(3, 3);
 
         }
     }
---
+++ FlashParkMVPTest/TestDbAsyncQueryProvider.cs
@@ -0,0 +1,139 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlashParkMVPTest
+{
+    internal class TestDbAsyncQueryProvider<TEntity> : IDbAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        internal TestDbAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new TestDbAsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new TestDbAsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _inner.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+
+        public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute(expression));
+        }
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute<TResult>(expression));
+        }
+    }
+
+    internal class TestDbAsyncEnumerable<T> : EnumerableQuery<T>, IDbAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestDbAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestDbAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IDbAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            return new TestDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
+        {
+            return GetAsyncEnumerator();
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new TestDbAsyncQueryProvider<T>(this); }
+        }
+    }
+
+    internal class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestDbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+
+    public static class QueryableExtensions
+    {
+        public static IDbSet<T> BuildMockDbSet<T>(this IQueryable<T> source)
+            where T : class
+        {
+            var mock = new Mock<IDbSet<T>>();
+            mock.As<IDbAsyncEnumerable<T>>()
+                .Setup(x => x.GetAsyncEnumerator())
+                .Returns(new TestDbAsyncEnumerator<T>(source.GetEnumerator()));
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.Provider)
+                .Returns(new TestDbAsyncQueryProvider<T>(source.Provider));
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.Expression)
+                .Returns(source.Expression);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.ElementType)
+                .Returns(source.ElementType);
+
+            mock.As<IQueryable<T>>()
+                .Setup(x => x.GetEnumerator())
+                .Returns(source.GetEnumerator());
+
+            return mock.Object;
+        }
+    }
+}
